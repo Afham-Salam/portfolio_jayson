bbca35e8a05fb30081594549eed2840780b8fe4c
Afham salam <afhamsalam8060@gmail.com>
2024-11-07 19:40:32 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-
-
 import About from '../components/About'
 import Skill from '../components/Skill'
 import Project from '../components/Project'
 import Contact from '../components/Contact'
 import Navbar from '../components/Navbar'
 import Profile from '../components/Profile'
-
-
 
 export default function Home() {
   return (
